9577e92019a331ae1e94456a34c7f76b21302457
Francois Leparoux <francois.leparoux@gmail.com>
2017-07-17 18:03:53 +0100

    Testfile updated

--- test.js
+++ test.js
@@ -6,6 +6,23 @@ var hm = new HubManager(1883, 27017);
 
 console.log(hm);
 
+// add a connection auth callback
+hm.authorizeClientConnection(
+	function (client, username, password) { 
+		return true; 
+	}
+);
+hm.authorizeClientSubscription(
+	function (client, topic) { 
+		return true; 
+	}
+);
+hm.authorizeClientPublish(
+	function (client, topic, payload) { 
+		return true 
+	}
+);
+
 // Setup the manager
 hm.setup();
 
