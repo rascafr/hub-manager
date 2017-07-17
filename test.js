25d858316c47c69af3acb5bc253f363155060aac
Francois Leparoux <francois.leparoux@gmail.com>
2017-07-17 16:48:50 +0100

    Test file updated

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
-var hubmanager = require('./index.js');
+// Include the manager
+var HubManager = require('./index.js');
 
-console.log('hello');
+// Create an instance that will listen on the port 1883
+var hm = new HubManager(1883, 27017);
+
+// Setup the manager
+hm.setup();
