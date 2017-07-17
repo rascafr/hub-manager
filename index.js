8c4eb491e8fda8e8ea17bdd3e09b31b43aba44fb
Francois Leparoux <francois.leparoux@gmail.com>
2017-07-17 17:36:34 +0100

    test debug ahah

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ class HubManager {
      */
     __broker_setup() {
         console.log(this);
-        /*this.mqttServer.authenticate =               this.__broker_auth;
+        this.mqttServer.authenticate =               this.__broker_auth;
         this.mqttServer.authorizePublish =           this.__broker_allow_sub;
         this.mqttServer.authorizeSubscribe =         this.__broker_allow_sub;
         this.mqttServer.on('clientConnected',        this.__broker_connected);
@@ -221,7 +221,7 @@ class HubManager {
         this.mqttServer.on('subscribed',             this.__broker_subscribed);
         this.mqttServer.on('unsubscribed',           this.__broker_unsubscribed);
         this.mqttServer.on('clientDisconnecting',    this.__broker_disconnecting);
-        this.mqttServer.on('clientDisconnected',     this.__broker_disconnected);*/
+        this.mqttServer.on('clientDisconnected',     this.__broker_disconnected);
         console.log('[MQTT] Mosca server is up and running on port ' + this.mqttPort);
     }
 
