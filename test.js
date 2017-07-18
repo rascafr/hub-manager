7c38f71589afab8b7a790ef3e1d226cadfb58066
Francois Leparoux <francois.leparoux@gmail.com>
2017-07-18 10:28:45 +0100

    Test file updated, package.json as well

--- test.js
+++ test.js
@@ -4,31 +4,44 @@ var HubManager = require('./index.js');
 // Create an instance that will listen on the port 1883
 var hm = new HubManager(1883, 27017);
 
-console.log(hm);
-
+/**
+ * Authentication callback
+ * @return true to accept the client connection, false otherwise
+ */
 var auth_conn = function (client, username, password) { 
-	return true; 
+  // check the database...
+  // if (client == 'guy') ...
+  // password.toString() might be needed as well if compared to a db result
+  return true; 
 }
 
+/**
+ * Subscription accept callback
+ * @return true to allow the client to subscribe on this topic, false otherwise
+ */
 var auth_subs = function (client, topic) { 
-	return true; 
+  return true; 
 }
 
+/**
+ * Publish accept callback
+ * @return true to allow the client to publish to a topic, false otherwise
+ */
 var auth_publ = function (client, topic, payload) { 
-	return true 
+  return true 
 }
 
-// add a connection auth callback
+// Set the connection / sub / pub accept callbacks
 hm.authorizeClientConnection(auth_conn);
 hm.authorizeClientSubscription(auth_subs);
 hm.authorizeClientPublish(auth_publ);
 
-// add a ready callback
+// Add a ready callback
 hm.onServerReady(function() {
-	console.log('Server is ready :)');
+	console.log('Server is ready yeah');
 })
 
-// Setup the manager
+// Setup the MQTT broker
 hm.setup();
 
 // ...
